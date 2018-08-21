5a871078393c3b5ed06cf053a2df450a2e466c26
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-20 22:21:12 -0700

    Add files via upload

---
+++ CNIT103Template.py
@@ -0,0 +1,135 @@
+#==================================================================
+#			            Computer Hardware
+#==================================================================
+
+''' 
+Date    : Mon 20 Aug 2018 09:43:43 PM PDT 
+Subject : CNIT 103 Computer Hardware
+Title   : Chapter 1
+Source  : 
+''' 
+
+#
+
+'''
+NOTES:
+
+'''
+
+# Chapter 1 
+
+'''
+PG 1
+Introduction to Computer User Support
+
+In this chapter, you will learn about:
+	1 How changes in computer technology  over time have affected computer use
+	2 Ways to classify end users
+	3 Resources computer users need 
+	4 Major categories of end-user need
+	5 Common problems encountered by users
+	6 Job market demandfir user support workers
+	7 COmmon ways to organixe and provude support services
+	8 Typical position desciptions for user support staff
+	9 Knowledge, skills, and abilities for entry-level support positions
+   10 Career paths for user support workers
+
+'''
+
+'''
+PG2
+The computer undustry has been vhanging continuosly since it began over 60 years ago, and 
+it will undoubtedly continue to change in the future. One result of this ongoing 
+transformation is that computer industry ptofessionals tately woek with the dame hardware,
+software, networksoperational procedures, and job descriptions they also need to learn how to keep
+up with constant change and how ro make learning new technologies a part of their day-to-
+day work routine.
+
+The purpose of this book is to help you learn abour and prepare for information technology
+jobs, with a focus on the problem that your xoworkers or clients will encounter when 
+they attempt to make effective use of technology. The book includes information on what 
+tou need to know and what you need to be able to do in order to work in the field of 
+cimputer user support. Chapter 1 provides an overview of information texhnology from 
+two perspectives. We use technology for work or personal activities. We will also 
+introduce computer support professional. To help you understand 
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
