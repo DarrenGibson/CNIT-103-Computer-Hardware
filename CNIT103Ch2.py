7e0b39fdd68c37b4e0647d8a5e76400e39fd2a4a
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-29 19:20:13 -0700

    Add files via upload

---
+++ CNIT103Ch2.py
@@ -0,0 +1,158 @@
+#==================================================================
+#			              Networking
+#==================================================================
+
+''' 
+Date    : Sun 26 Aug 2018 04:34:06 PM PDT 
+Subject : CNIT 106 Networking
+Title   : Working Inside Desktop Computers and Laptops
+Source  : Textbook: A+ Guide to IT Technical Support
+''' 
+
+# Intro 
+
+'''
+NOTES:
+
+'''
+
+# Chapter 2
+# PG 45 - 78
+
+'''
+Notes
+
+Chapter Summary
+
+How to work unside a desktop computer case
+
+    When a hardware support technician is disassembling or reassembling a computerm it is important to stay
+    organized, keep careful notes, and follow all the safety procedures ro protect the computer equipment. 
+    
+	Before opening a computer case, shut down the system, unplug it, disconnect all the cables, and press the 
+	power button to drain residual power.
+	
+Special consideration when supporting laptops
+
+	laptop computers are designed for travel. They use the same technology as desktop computers, with 
+	modificatoinis for space, portability, and power consevation. A laptop generally costs more than a 
+	desktop with comparable power and features. Special concerns when supporting a laptop also apply
+	to supporting a netbook or all-in one computer. 
+	
+	When supporting laptops, pay careful attention to what the warranty allows you to change on the computer.
+	
+	The laptop manufacturer documentation, including the service manual, diagnostic software, and 
+	recovery media, is useful when disassembling, troubleshooting, and repairing a laptop.
+	
+How to work inside a laptop computer 
+
+	Field resplaceable units in a laptop can include the memory modules, hard drive, LCD panel, video
+	inverter, keyboard, touch pad, processor, optucal drive, DC jack fan, motherboard, CMOS battery, 
+	Mini-PCIe card, wireless card, or speakers. 
+	
+	When an internal component needs replacing, consider the possibility of disabling the component and 
+	using an external peripheral device in its place. Don't jeoperdize the warrenty on laptop by opening 
+	the care or using components not authorized by the manufactorer.
+	
+	Replacing the laptop might be more cost effective than performing labor-intensive repairs, such as 
+	replacing the motherboard.  
+	
+	When disassembling a laptop, the manufacturer's sevice id essential.
+	
+	When upgrading components on a laptop, including memory, use components that are the same brand
+	as the laptop, or use only components recommended by the laptop's mamufacturer.
+	
+	Follow the directions in a sevice manial to disassemble a laptop. Heep small screws organized as 
+	you dis assemble a laptop because you use new screws rather than the old ones.
+	
+	
+	
+Key Terms
+
+	front panel connector 
+	front panel header spacer
+	stanoff 
+	ZIF connector 
+	
+	
+	
+Reviewing the basics 
+	
+	When taking a computer apart, why is it imporptant to not stack boards
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+  
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
